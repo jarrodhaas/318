f1e91a8ba6b0dacbc3bdb0a920b6beccaeebd3a4
jhaas <jhaas@sfu.ca>
2017-07-27 21:59:08 -0700

    fixed typo

--- hmmtest2.py
+++ hmmtest2.py
@@ -19,7 +19,7 @@ pandas2ri.activate()
 # read data
 print("\nReading data...\n")
 parser = lambda date: pd.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-data = pd.read_csv("sorted.csv", nrows=200000, parse_dates=[1], date_parser=parser, index_col=0)
+data = pd.read_csv("sorted.csv", nrows=300000, parse_dates=[1], date_parser=parser, index_col=0)
 
 
 def findDateIndexOld(date):
@@ -85,139 +85,137 @@ def getChunk(start_date, len_days, csv_name):
     return
 
 
-
-
 #########################
 # Compare LL of different day periods
 #########################
 
-def compareLLModels():
-    # get reference window data for training
-    print("\nGetting reference windows...\n")
 
-    #
-    # parameters
-    #
+# get reference window data for training
+print("\nGetting reference windows...\n")
 
-    test_date = dt.date(2007, 2, 28)
-    #0700
-    day_start = 420
-    #1900
-    day_end = 1140
+#
+# parameters
+#
 
-    # number of reference windows
-    num_ref = 3
-    # intervals -- 7 is same day previous week, 1 is previous day
-    win_type = 7
+test_date = dt.date(2007, 5, 20)
+#0700
+day_start = 420
+#1900
+day_end = 1140
 
-    #
-    # End parameters
-    #
+# number of reference windows
+num_ref = 3
+# intervals -- 7 is same day previous week, 1 is previous day
+win_type = 7
 
-    rw,tw = getRefWindow(num_ref, win_type, test_date)
+#
+# End parameters
+#
 
-    # move the dataframes over to R
+rw,tw = getRefWindow(num_ref, win_type, test_date)
 
-    #rw = rw.reshape((rw.shape[0],))
-    #r_rw = ro.numpy2ri.py2ri(rw)
-    # r_tw = pandas2ri.py2ri(tw)
+# move the dataframes over to R
 
-    #note: need to swap order of these later!
-    #also: need to set N to proper length for each rw!!
+#rw = rw.reshape((rw.shape[0],))
+#r_rw = ro.numpy2ri.py2ri(rw)
+# r_tw = pandas2ri.py2ri(tw)
 
-    #r_rwlist = r.list(N=780, x=r_rw)
+#note: need to swap order of these later!
+#also: need to set N to proper length for each rw!!
 
-    #def testLL():
+#r_rwlist = r.list(N=780, x=r_rw)
 
-    # send this to R
-    rstring="""
-        function(){
+#def testLL():
 
-            library("mhsmm")
+# send this to R
+rstring="""
+    function(){
 
-            # grab the testing window data -- this acts as our baseline
-            dataT = as.matrix(read.csv("/Users/JRod/Desktop/Summer 2017/318D2/project/testwin.csv"))
-            dataT = as.numeric(dataT)
-            baseline = list(x=dataT, N=length(dataT))
-            #train_t <- rw_list[c("x","N")]
-            class(baseline) <- "hsmm.data"
+        library("mhsmm")
 
-            # initialize values for learning the HSMM
+        # grab the testing window data -- this acts as our baseline
+        dataT = as.matrix(read.csv("/Users/JRod/Desktop/Summer 2017/318D2/project/testwin.csv"))
+        dataT = as.numeric(dataT)
+        baseline = list(x=dataT, N=length(dataT))
+        #train_t <- rw_list[c("x","N")]
+        class(baseline) <- "hsmm.data"
 
-            J <- 4
-            M = 150
+        # initialize values for learning the HSMM
 
-            d <- cbind(dunif(1:M,0,150),
-                       dunif(1:M,0,150),
-                       dunif(1:M,0,150),
-                       dunif(1:M,0,150))
+        J <- 4
+        M = 150
 
-            start.np <- hsmmspec(init=rep(1/J,J),
-                                 transition=matrix(c(.00,.5,.5,.5,
-                                                     .5,.00,.5,.5,
-                                                     .5,.50,.00,.50,
-                                                     .5,.50,.50,.00),nrow=J),
-                                 parms.emission=list(mu=c(0.5, 1.5, 2.5, 3.5),
-                                                     sigma=c(0.75, 0.5, 0.5, 0.5)),
-                                 sojourn=list(d=d,type='nonparametric'),
-                                 dens.emission=dnorm.hsmm)
+        d <- cbind(dunif(1:M,0,150),
+                   dunif(1:M,0,150),
+                   dunif(1:M,0,150),
+                   dunif(1:M,0,150))
 
-            #fit the model and predict
-            h.act <- hsmmfit(baseline,start.np,mstep=mstep.norm,M=M,graphical=FALSE)
-            predicted <- predict(h.act,baseline)
+        start.np <- hsmmspec(init=rep(1/J,J),
+                             transition=matrix(c(.00,.5,.5,.5,
+                                                 .5,.00,.5,.5,
+                                                 .5,.50,.00,.50,
+                                                 .5,.50,.50,.00),nrow=J),
+                             parms.emission=list(mu=c(0.5, 1.5, 2.5, 3.5),
+                                                 sigma=c(0.75, 0.5, 0.5, 0.5)),
+                             sojourn=list(d=d,type='nonparametric'),
+                             dens.emission=dnorm.hsmm)
+
+        #fit the model and predict
+        h.act <- hsmmfit(baseline,start.np,mstep=mstep.norm,M=M,graphical=FALSE)
+        predicted <- predict(h.act,baseline)
 
 
-            ll <- predicted$loglik
+        ll <- predicted$loglik
 
-            for (i in 0:""" + str(num_ref-1) + """) {
+        for (i in 0:""" + str(num_ref-1) + """) {
 
-              # grab the test data and compare to baseline
-              data1 = as.matrix(read.csv(paste("/Users/JRod/Desktop/Summer 2017/318D2/project/ref", i, ".csv", sep="")))
-              data1 = as.numeric(data1)
-              train = list(x=data1, N=length(data1))
-              class(train) <- "hsmm.data"
+          # grab the test data and compare to baseline
+          data1 = as.matrix(read.csv(paste("/Users/JRod/Desktop/Summer 2017/318D2/project/ref", i, ".csv", sep="")))
+          data1 = as.numeric(data1)
+          train = list(x=data1, N=length(data1))
+          class(train) <- "hsmm.data"
 
-              predicted <- predict(h.act,train)
+          predicted <- predict(h.act,train)
 
-              ll <- c(ll,predicted$loglik)
-            }
-
-            ll
+          ll <- c(ll,predicted$loglik)
         }
-    """
+
+        ll
+    }
+"""
 
 
-    # put the string into a function
-    rfunc=ro.r(rstring)
-    # call the function in R and return the log likelyhood
-    loglik=rfunc()
+# put the string into a function
+rfunc=ro.r(rstring)
+# call the function in R and return the log likelyhood
+loglik=rfunc()
 
-    # calculate some descriptive info from our data
-
-
-    loglik = np.asarray(loglik)
-    baseline = loglik[0]
-
-    loglik = loglik[1:]
-
-    dist = abs(loglik - baseline)
-    avg = np.mean(dist)
-    mx = dist[np.argmax(dist)]
-    mn = dist[np.argmin(dist)]
-    std = np.std(dist)
-
-    output = pd.DataFrame({'date': [test_date],'avg':[avg], 'std':[std], 'max':[mx], 'min':[mn], 'baseline':[baseline], 'win_type' : [win_type], 'num_ref' : [num_ref]})
-    # resort headers
-    output = output[['date','num_ref', 'win_type', 'baseline', 'avg', 'std', 'max', 'min']]
-
-    # changes the order for some reason to avg, max, min, std
-    output.to_csv('results.csv', header=False, index=False, mode='a')
+# calculate some descriptive info from our data
 
 
-    print("baseline date: ", test_date, "rw type: ", win_type)
-    print("baseline: ", baseline)
-    print("output: ", loglik)
-    print("avg dist: ", avg)
-    print("std dev: ", std)
-    print("max dist: ", mx)
-    print("min dist: ", mn)
+loglik = np.asarray(loglik)
+baseline = loglik[0]
+
+loglik = loglik[1:]
+
+dist = abs(loglik - baseline)
+avg = np.mean(dist)
+mx = dist[np.argmax(dist)]
+mn = dist[np.argmin(dist)]
+std = np.std(dist)
+
+output = pd.DataFrame({'date': [test_date],'avg':[avg], 'std':[std], 'max':[mx], 'min':[mn], 'baseline':[baseline], 'win_type' : [win_type], 'num_ref' : [num_ref]})
+# resort headers
+output = output[['date','num_ref', 'win_type', 'baseline', 'avg', 'std', 'max', 'min']]
+
+# changes the order for some reason to avg, max, min, std
+output.to_csv('results.csv', header=False, index=False, mode='a')
+
+
+print("baseline date: ", test_date, "rw type: ", win_type)
+print("baseline: ", baseline)
+print("output: ", loglik)
+print("avg dist: ", avg)
+print("std dev: ", std)
+print("max dist: ", mx)
+print("min dist: ", mn)
